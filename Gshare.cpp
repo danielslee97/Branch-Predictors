da13318fe375209a93b945e2b0bd55e18b52c322
Seung Woo Lee <danielslee97@gmail.com>
2017-10-29 22:38:44 -0400

    Add files via upload

--- Gshare.cpp
+++ Gshare.cpp
@@ -1,26 +1,22 @@
 #include "Gshare.h"
 #include <cmath>
-#include <iostream>
 
 Gshare::Gshare() {
 	this->history = 0x00000000000;
 	this->size = 2048;
-	this->historySize = pow(2, 11);
-	this->numBits = log2(2048);
+	this->historySize = long(pow(2, 11));
 }
 Gshare::Gshare(float size) {
 	this->history = 0x00000000000;
 	this->size = 2048;
-	this->historySize = int(pow(2, size));
-	this->numBits = log2(2048);
+	this->historySize = long(pow(2, size));
 }
-bool Gshare::predict(int address, string branch) {
+bool Gshare::predict(long address, string branch) {
 	bool outcome;
-	int signBits = address % int(pow(2.0, float(this->numBits)));
+	long signBits = address % this->size; 
 	
-	if(signBits > this->size) throw 0;
 	if(branch == "T") outcome = true;
-	if(branch == "NT") outcome = false;
+	else if(branch == "NT") outcome = false;
 	
 	int location = signBits ^ this->history;
 	if(this->table.find(location) == this->table.end()) {
@@ -28,23 +24,15 @@ bool Gshare::predict(int address, string branch) {
 	}
 	
 	bool prediction;
-	if(this->table.at(location) >= 2) {
-		prediction = true;
-	}
-	else {
-		prediction = false;
-	}
-	//cout << "PREDICTION BEFORE: " << this->table.at(location) << endl;
-	//cout << "HISTORY BEFORE: " << this->history << endl;
+	if(this->table.at(location) >= 2) prediction = true;
+	else prediction = false;
+
 	if(prediction == outcome) {
 		if(this->table.at(location) == 2) this->table.at(location)++;
-		else this->table.at(location)--;
+		else if(this->table.at(location) == 1) this->table.at(location)--;
 		this->history = this->history << 1;
 		if(outcome) this->history++;
-		//cout << "HISTORY AFTER: " << this->history << endl;
 		if(this->history >= this->historySize) this->history -= this->historySize;
-		//cout << "HISTORY AFTER AFTER: " << this->history << endl;
-		//cout << "PREDICTION AFTER: " << this->table.at(location) << '\n' << endl;
 		return true;
 	}
 	else {
@@ -52,10 +40,7 @@ bool Gshare::predict(int address, string branch) {
 		else this->table.at(location)++;
 		this->history = this->history << 1;
 		if(outcome) this->history++;
-		//cout << "HISTORY AFTER: " << this->history << endl;
 		if(this->history >= this->historySize) this->history -= this->historySize;
-		//cout << "HISTORY AFTER AFTER: " << this->history << endl;
-		//cout << "PREDICTION AFTER: " << this->table.at(location) << '\n' << endl;
 		return false;
 	}
 }
